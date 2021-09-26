f0939933c087fce6e601125000d3100604a72af1
lalami3232 <abudhabibruu@gmail.com>
2021-09-26 14:44:44 +0100

    school

--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "school_app/index.html")
+
+def login(request):
+    return render(request, "school_app/login.html")
